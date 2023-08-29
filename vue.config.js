ca470ff58d802f7f084ad97bbedad20f3216d484
AndreiOp235 <cosaandrei90@gmail.com>
2023-08-29 10:04:20 +0100

    Add deploy workflow

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+
+
+<pre data-v-03c63cf6="" id="js">module.exports = defineConfig({
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/bootcamp-sts-2023/' // Numele repository-ului creat.
+    : '/'
+})</pre>
