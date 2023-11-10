652ed8ae3e9345dbe53c0a12a1510921dd16423a
Coșa Andrei Valeriu <42703353+AndreiOp235@users.noreply.github.com>
2023-11-10 21:05:31 +0200

    Update HelloWorld.vue
    Added image caroussel

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -27,16 +27,43 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
+
+    <!-- Image Carousel -->
+    <h3>Image Carousel</h3>
+    <carousel :perPage="1" :navigationEnabled="true">
+      <!-- Carousel Slides -->
+      <slide v-for="(image, index) in carouselImages" :key="index">
+        <img :src="image.src" :alt="image.alt">
+      </slide>
+    </carousel>
   </div>
 </template>
 
 <script>
+import { Carousel, Slide } from 'vue-carousel';
+
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+  components: {
+    Carousel,
+    Slide,
+  },
+  data() {
+    return {
+      carouselImages: [
+        { src: 'https://media.licdn.com/dms/image/D4D22AQE5rMizVlUFLA/feedshare-shrink_2048_1536/0/1693558425986?e=1702512000&v=beta&t=4j4_byxFupGofkhEuArqgbuV8VvfSJ2UXmuvdKGKrNk', alt: 'Image 1' },
+        { src: 'https://media.licdn.com/dms/image/D4D22AQGyHahaXg-WjQ/feedshare-shrink_2048_1536/0/1693638324907?e=1702512000&v=beta&t=mviGlqBIgQAAwef9fiTvaz9qTdN_FQhEo8lC7lwrtls', alt: 'Image 2' },
+        { src: 'https://media.licdn.com/dms/image/D4D22AQEILC3cQn9q9A/feedshare-shrink_2048_1536/0/1693334389420?e=1702512000&v=beta&t=_yXcY77N-tw2y-3yzcyHDFE9bFETUuhOm_REo0xH-g8', alt: 'Image 3' },
+        { src: 'https://media.licdn.com/dms/image/D4D22AQGlnJ0t8OlcLw/feedshare-shrink_2048_1536/0/1693240693365?e=1702512000&v=beta&t=D51qhRD2ZSjRBGBv6FRQXoDPw84dVU0wfPSrFoKSN5A', alt: 'Image 4' },
+        { src: 'https://media.licdn.com/dms/image/D4D22AQFzqOptlRmYXg/feedshare-shrink_2048_1536/0/1693240693948?e=1702512000&v=beta&t=WaDkiGYo5qAnRB7b6Zp6uaS2gbDHVoTF3ujBf5mk2Dk', alt: 'Image 5' },
+        // Add more image links as needed
+      ],
+    };
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -54,5 +81,17 @@ li {
 }
 a {
   color: #42b983;
+}
+
+/* Styles for the Image Carousel */
+.carousel {
+  width: 100%;
+  max-width: 600px; /* Adjust the maximum width as needed */
+  margin: 20px auto;
+}
+
+.carousel img {
+  width: 100%;
+  height: auto;
 }
 </style>
